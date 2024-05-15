d174da7d5d27508dc6708e2c4a121fa521d51540
solchan <dbsthfcks123@gmail.com>
2024-05-15 14:44:38 +0900

    프로젝트 생성

---
+++ src/main/java/com/project/scmovie/scmovie/ScmovieApplication.java
@@ -0,0 +1,13 @@
+package com.project.scmovie.scmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScmovieApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/scmovie/scmovie/ScmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.scmovie.scmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
